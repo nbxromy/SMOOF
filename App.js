25bd2d67d26681a542a769b3f5a3d3eef5560c92
HDhian <56302681+HDhian@users.noreply.github.com>
2022-05-24 15:40:13 +0200

    graph

--- App.js
+++ App.js
@@ -23,10 +23,21 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { StyleSheet, View, Text, Image, Button } from 'react-native';
+import { StyleSheet, View, Dimensions, SafeArea, Text, Image, Button } from 'react-native';
 import VerticalBarGraph from '@chartiful/react-native-chart-builder/lib/types';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Agenda } from 'react-native-calendars'
+import { useSafeAreaFrame } from 'react-native-safe-area-context';
+import { useState } from 'react/cjs/react.production.min';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
 
 function HomeScreen({ navigation }) {
   return (
@@ -34,7 +45,7 @@ function HomeScreen({ navigation }) {
       <Image source={{
         width: 250,
         height: 250,
-        uri: 'https://cdn-icons.flaticon.com/png/512/6070/premium/6070321.png?token=exp=1652846128~hmac=9f435e53793cd79231d3986f73b06b29'}} />
+        uri: 'https://cdn-icons.flaticon.com/png/512/6070/premium/6070321.png?token=exp=1653387289~hmac=5797cefd574734689658241200e11415'}} />
       <Text style={styles.kleurGroen}>Please connect your device</Text>
       <Button
         title="Connect to the app"
@@ -47,8 +58,8 @@ function HomeScreen({ navigation }) {
 function ProfileScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center' , marginTop: 25 }}>
-      <Text style={styles.kleurBlue}>Je data</Text>
-      <View style={{flex: 1, right: 100, margin: 40}}>
+      <Text style={styles.kleurBlue}>Your data</Text>
+      <View style={{ marginTop: 30,alignItems: 'center', justifyContent: 'flex-start', flexDirection: 'row'}}>
       <Image 
         style= {{
           width: 50,
@@ -59,7 +70,8 @@ function ProfileScreen() {
           uri: 'https://cdn-icons.flaticon.com/png/512/1818/premium/1818145.png?token=exp=1652867719~hmac=bbb63cc9acd2dfca5ddb75fb0d25584c'
         }}
         />
-      <Text style={styles.kleurGroen}>0 bps</Text>
+        
+      <Text>       </Text>
       <Image 
         style= {{
           width: 50,
@@ -70,13 +82,94 @@ function ProfileScreen() {
           uri: 'https://cdn-icons.flaticon.com/png/512/2992/premium/2992422.png?token=exp=1652867764~hmac=6e92bf36deb70091a9877462327448f9'
         }}
         />
-      <Text style={styles.kleurGroen}>0 hour(s)</Text>
       
       </View>
+      <View style={{flexDirection: 'row', justifyContent: 'flex-start'}}>
+      <Text style={styles.kleurGroen}>{heartRate} bps</Text>
+      <Text>     </Text>
+      <Text style={styles.kleurGroen}>{slaap}hours</Text>
+      </View>
+      
+      <LineChart
+    data={{
+      labels: ["Sun","Mon", "Tue", "Wed", "Thu", "Fri", "Sa"],
+      datasets: [
+        {
+          data: [0,0,0,0,0,0,0]
+        }
+      ]
+    }}
+    width={Dimensions.get("window").width} // from react-native
+    height={200}
+    yAxisLabel=""
+    yAxisSuffix="°C"
+    yAxisInterval={1} // optional, defaults to 1
+    chartConfig={{
+      backgroundColor: "#e26a00",
+      backgroundGradientFrom: "#fb8c00",
+      backgroundGradientTo: "#ffa726",
+      decimalPlaces: 0, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16, 
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        stroke: "#ffa726"
+      }
+    }}
+    bezier
+    style={{
+      marginTop: 20,
+      marginVertical: 8,
+      borderRadius: 16
+    }}
+  />
+  <BarChart
+    data={{
+      labels: ["Sun","Mon", "Tue", "Wed", "Thu", "Fri", "Sa"],
+      datasets: [
+        {
+          data: [6,6.5,7,7.25,5.75,7,6]
+        }
+      ]
+    }}
+    width={Dimensions.get("window").width} // from react-native
+    height={220}
+    yAxisLabel=""
+    yAxisSuffix=" Hr(s)"
+    yAxisInterval={1} // optional, defaults to 1
+    chartConfig={{
+      backgroundColor: "#eee",
+      decimalPlaces: 1, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        stroke: "#ffa726"
+      }
+    }}
+    style={{
+      marginVertical: 8,
+      borderRadius: 16
+    }}
+  />
     </View>
+    
+
+    
   
   );
 }
+
+var heartRate = 0 ;
+var slaap = 0 ;
 
 const Stack = createNativeStackNavigator();
 
